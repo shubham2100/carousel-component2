8d7951926152077332023518b76d6bf47cfdda76
Shubham Garg <shubhamgarg2100@gmail.com>
2022-09-01 12:01:05 +0530

    updated the label

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <li className="list-elements"><a className="white-color" href="https://www.google.com/">Send money</a></li>
           <li className="list-elements yellow-color"><button onClick={handleClick}><FaBars/></button></li>
           <li className="list-elements yellow-color"><FaGlobe/></li>
-          <li className="list-elements"><a className="white-color" href="https://www.google.com/">FR</a></li>
+          <li className="list-elements"><a className="white-color" href="https://www.google.com/">Find Result</a></li>
           <li className="list-elements"><a className="yellow-color" href="https://www.google.com/">Register</a></li>
           <li className="list-elements"><a className="yellow-color" href="https://www.google.com/">To Log in</a></li>
         </ul>
